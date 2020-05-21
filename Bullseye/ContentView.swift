66d7fcd49b7df06e7884dfe270f572a45b6ca8d8
Atilhaam <ilham.wayne@gmail.com>
2020-05-21 20:22:46 +0700

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Ilham on 21/05/20.
+//  Copyright © 2020 Ilham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
